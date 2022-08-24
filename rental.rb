b6e677c809887b8d2e0c40daeeffa0ca86d5919d
Waleed Amjad <waleedamjad99@hotmail.com>
2022-08-24 22:15:40 +0500

    Add rental .rb

---
+++ rental.rb
@@ -0,0 +1,19 @@
+class Rental
+  attr_accessor :date
+
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
+  end
+
+  def book=(book)
+    @book = book
+    book.rentals.push(self) unless book.rentals.include?(self)
+  end
+
+  def person=(person)
+    @person = person
+    person.rentals.push(self) unless person.rentals.include?(self)
+  end
+end

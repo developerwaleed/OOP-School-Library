945f7c572d6ed66a5e8636d642a172012a291c5f
Waleed Amjad <waleedamjad99@hotmail.com>
2022-08-25 08:04:54 +0500

    Replace require with require relative

--- book.rb
+++ book.rb
@@ -1,3 +1,5 @@
+require_relative 'rental'
+
 class Book
   attr_accessor :title, :author, :rentals
 
@@ -7,8 +9,7 @@ class Book
     @rentals = []
   end
 
-  def add_rental(rental)
-    @rentals.push(rental)
-    rental.Book = self
+  def add_rental(date, person)
+    Rental.new(date, person, self)
   end
 end

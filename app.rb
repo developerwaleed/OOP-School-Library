95969dda015f413a70c118c3029d081b08c264cf
Waleed Amjad <waleedamjad99@hotmail.com>
2022-08-26 16:51:00 +0500

    Add renta and list people methods

--- app.rb
+++ app.rb
@@ -27,13 +27,13 @@ class App
         when 1
             list_books
         when 2
-            puts "\nYour List of Poeple is:"
+            list_people
         when 3
-            puts "\nLets create a person"
+            create_person
         when 4
             create_book
         when 5
-            puts "\nLets create a Rental"
+            create_rental
         when 6
             puts "\nYour List of Rental is:"
         when 7
@@ -73,4 +73,83 @@ class App
         end
       end
 
+    def create_person
+        puts
+        print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+        choice = gets.chomp.strip.to_i
+        case choice
+        when 1
+          create_student
+        when 2
+          create_teacher
+        end
+    end
+
+    def create_student
+        puts
+        print 'Age: '
+        age = gets.chomp.strip.to_i
+        while age <= 0 || age >= 100
+          print 'Please input a valid age: (1 - 100): '
+          age = gets.chomp.strip.to_i
+        end
+        print 'Name: '
+        name = gets.chomp.strip.capitalize
+        print 'Has parent permission? [Y/N]: '
+        permission = gets.chomp.strip.upcase
+        case permission
+        when 'Y'
+          permission = true
+        when 'N'
+          permission = false
+        end
+        @people << Student.new(age, nil, name, parent_permission: permission)
+        puts 'Person created successfully'
+    end
+
+    def create_teacher
+        puts
+        print 'Age: '
+        age = gets.chomp.strip.to_i
+        while age <= 0 || age >= 100
+          print 'Please input a valid age (1 - 100): '
+          age = gets.chomp.strip.to_i
+        end
+        print 'Name: '
+        name = gets.chomp.strip.capitalize
+        print 'Specialization: '
+        specialization = gets.chomp.strip
+        @people << Teacher.new(age, specialization, name)
+        puts 'Person created successfully'
+    end
+
+    def create_rental
+        puts
+        puts 'Select a book from the following list by number'
+        list_books
+        book_choice = gets.chomp.to_i
+        while book_choice.negative? || book_choice >= @books.length
+          print "Please enter a number within 0 - #{@books.length - 1} range: "
+          book_choice = gets.chomp.to_i
+        end
+        book = @books[book_choice]
+        puts 'Select a person from the following list by number (not id)'
+        list_people
+        person_choice = gets.chomp.to_i
+        while person_choice.negative? || person_choice >= @people.length
+          print "Please enter a number within 0 - #{@people.length - 1} range: "
+          person_choice = gets.chomp.to_i
+        end
+        person = @people[person_choice]
+        print 'Enter date of booking: (yyyy/mm/dd) : '
+        date = gets.chomp.strip
+        person.add_rental(date, book)
+    end
+
+    def list_people
+        puts
+        @people.each_with_index do |person, index|
+          puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id} Age: #{person.age}"
+        end
+      end
 end

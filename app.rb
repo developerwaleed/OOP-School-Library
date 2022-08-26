55bab2c169b7af7ef0fc99c92b088ebc6c4326e1
Waleed Amjad <waleedamjad99@hotmail.com>
2022-08-26 16:31:21 +0500

    Add Checks

--- app.rb
+++ app.rb
@@ -19,4 +19,27 @@ class App
         puts '7 - Exit'
     end
 
+    def check(options)
+        case options
+        when 1
+            puts "\nYour List of Books is:"
+        when 2
+            puts "\nYour List of Poeple is:"
+        when 3
+            puts "\nLets create a person"
+        when 4
+            puts "\nLets create a Book"
+        when 5
+            puts "\nLets create a Rental"
+        when 6
+            puts "\nYour List of Rental is:"
+        when 7
+            puts "\nGOOD BYE!"
+        else
+            puts "\nWrong Input!"
+        end
+    end
+
+    
+
 end

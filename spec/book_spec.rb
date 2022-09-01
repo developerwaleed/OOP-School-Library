23fd018f38d4cd542215f3381e3f35cc058370d8
Terbeche Mustapha <termus96@gmail.com>
2022-09-01 09:09:55 +0100

    Add test for Book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Book do
+  before :each do
+    @book = Book.new 'Title', 'Author'
+  end
+
+  describe '#new' do
+    it 'takes 2 parameters and returns a Book object' do
+      expect(@book).to be_instance_of Book
+    end
+  end
+
+  describe '#title' do
+    it 'returns the correct title' do
+      @book.title.should eql 'Title'
+    end
+  end
+  describe '#author' do
+    it 'returns the correct author' do
+      @book.author.should eql 'Author'
+    end
+  end
+end
